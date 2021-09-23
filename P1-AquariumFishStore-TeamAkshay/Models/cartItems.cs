d1d11f4b12fbc87eb2051f30df977d740b37cbe1
Shivani G <goswami.shivani.17et1091@gmail.com>
2021-09-23 17:03:01 +0530

    not required

---
+++ P1-AquariumFishStore-TeamAkshay/Models/cartItems.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace P1_AquariumFishStore_TeamAkshay.Models
+{
+    public class cartItems
+    {
+        public int Id { get; set; }
+
+        public int? LocationId { get; set; }
+
+        public int? UserId { get; set; }
+
+        public int? ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public string Location { get; set; }
+
+        public string Product { get; set; }
+
+        public string customer { get; set; }
+    }
+}
